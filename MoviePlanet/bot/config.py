768d1bc0158835667c972274537323354551204d
kanewi11 <blacknekit11@gmail.com>
2022-07-31 00:08:45 +0300

    Edit url site

--- MoviePlanet/bot/config.py
+++ MoviePlanet/bot/config.py
@@ -3,6 +3,6 @@ import os
 API_TOKEN = os.environ.get('TOKEN')
 
 # сюда добавить группы '@GroupName'
-SITE_URL = 'http://kingzmsk.ru'
+SITE_URL = 'https://kingzmsk.ru'
 MY_CHANNEL_URL = '@movies_from_planet'
 CHANNELS_TO_SUBSCRIBE = ['@movies_from_planet']
