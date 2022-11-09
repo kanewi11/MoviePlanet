cebc87fd554842706766c7f0c52831ab1a9cc0f2
kanewi11 <blacknekit11@gmail.com>
2022-11-09 06:59:24 +0300

    Improvements

--- MoviePlanet/bot/__init__.py
+++ MoviePlanet/bot/__init__.py
@@ -12,7 +12,6 @@ from aiogram import Bot, types
 
 from .config import API_TOKEN
 
-
 logging.basicConfig(level=logging.INFO, filename='logs.log', format='%(asctime)s %(levelname)s %(message)s')
 
 engine = create_engine('sqlite:///base.db', echo=False)
@@ -21,11 +20,16 @@ session = Session()
 
 cb = CallbackData('post', 'id', 'action')
 
-
 bot = Bot(token=API_TOKEN, parse_mode=types.ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
+WEBHOOK_HOST = 'https://kingzmsk.ru'
+WEBHOOK_PATH = '/webhook'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+WEBAPP_HOST = 'localhost'
+WEBAPP_PORT = 3001
 
 from .bot_func import *
 
@@ -34,15 +38,28 @@ async def on_bot_start_up(dispatcher: Dispatcher):
     asyncio.create_task(send_post())
 
 
-async def on_shutdown(disp):
+async def on_startup(dispatcher: Dispatcher):
+    await bot.set_webhook(WEBHOOK_URL)
+
+
+async def on_shutdown(dispatcher: Dispatcher):
     logging.warning('Shutting down..')
+
     await bot.delete_webhook()
 
-    await disp.storage.close()
-    await disp.storage.wait_closed()
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
 
     logging.warning('Bye!')
 
 
 def start_bot():
-    executor.start_polling(dp, skip_updates=True, on_startup=on_bot_start_up)
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
--- redirect_site/start_site.py
+++ redirect_site/start_site.py
@@ -1,15 +1,39 @@
-from flask import Flask, request, render_template, abort
+import json
+import logging
+import requests
+import traceback
 import validators
 
+from flask import Flask, request, render_template, jsonify, make_response
+
+
+AIOGRAM_PORT = 3001
+AIOGRAM_URL = f'http://localhost:{AIOGRAM_PORT}'
 
 application = Flask(__name__)
+
+
+@application.errorhandler(Exception)
+def not_found(error):
+    return render_template('not_found.html', error_code=error.code)
+
+
+@application.route('/webhook')
+def webhook():
+    if request.content_type == 'application/json':
+        try:
+            data = request.json
+            requests.post(AIOGRAM_URL, data=json.dumps(data), headers=request.headers, timeout=10)
+        except Exception:
+            logging.warning(traceback.format_exc())
+    return jsonify({'status': 'Ok'})
 
 
 @application.route('/')
 def index():
     q = request.args.get('q')
     if not q or not validators.url(q):
-        abort(404)
+        return render_template('hello.html')
     return render_template('index.html', q=q)
 
 
